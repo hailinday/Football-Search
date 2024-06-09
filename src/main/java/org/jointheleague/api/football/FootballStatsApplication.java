62bd78f7b7862a7ce626e2ae511bfdcb883a51e4
hailinday <54258211+hailinday@users.noreply.github.com>
2024-06-09 12:26:59 -0700

    initial

---
+++ src/main/java/org/jointheleague/api/football/FootballStatsApplication.java
@@ -0,0 +1,13 @@
+package org.jointheleague.api.football;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballStatsApplication.class, args);
+	}
+
+}
